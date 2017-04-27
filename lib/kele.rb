1f051423fe389897344719b18d8b426ede611570
Ryan Arakawa <arakawa.ryan@gmail.com>
2017-04-27 17:23:32 +0000

    User Story 3: Completed

--- lib/kele.rb
+++ lib/kele.rb
@@ -5,10 +5,23 @@ require "httparty"
 
 class Kele
   include HTTParty
-  BASE_URL = 'https://www.bloc.io/api/v1'
   def initialize(email, password)
-    response = self.class.post("#{BASE_URL}/sessions", body: { email: email, password: password })
+    response = self.class.post(api_url("sessions"), body: { email: email, password: password })
    raise "Invalid email address or password" if response.code != 200
     @auth_token = response["auth_token"]
+  end
+  
+  def get_me
+    response = self.class.get(api_url("users/me"), headers: { "authorization" => @auth_token })
+    @user_attr = JSON.parse(response.body)
+    @user_attr.keys.each do |key|
+      self.class.send(:define_method, key.to_sym) do
+        @user_attr[key]
+      end
+    end
+  end
+  
+  def api_url(end_point)
+    "https://www.bloc.io/api/v1/#{end_point}"
   end
 end
